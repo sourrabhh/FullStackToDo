5f770d5b8a2dcbc8c7e4cfb6cabf8413bf63c514
Sourabh <souraabh@yahoo.com>
2024-09-10 10:10:00 +0530

    cache removed

---
+++ todo_backend/src/test/java/com/sourabh/todo/TodoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sourabh.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+
+@SpringBootTest
+@ActiveProfiles("test")
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
