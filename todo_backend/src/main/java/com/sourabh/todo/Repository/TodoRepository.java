4d3516d3f11227404fd319e6a3f8b51201aa7f41
Sourabh <souraabh@yahoo.com>
2024-09-10 10:09:27 +0530

    cache removed

--- todo_backend/src/main/java/com/sourabh/todo/Repository/TodoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sourabh.todo.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sourabh.todo.Model.Todo;
-
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-}
